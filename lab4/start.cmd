23585810b5297e646439028ceaa147b0bbe08212
Георгий <George Krasavin>
2024-12-21 00:38:06 +0400

    fix bugs

---
+++ lab4/start.cmd
@@ -0,0 +1,12 @@
+@echo off
+echo Запуск JSON Server...
+start "JSON Server" cmd /k "json-server --watch data.json --port 3000"
+
+echo Запуск HTTP Server...
+start "HTTP Server" cmd /k "http-server -c-1 -p 8000"
+
+echo Открытие сайта в браузере...
+start "" "http://localhost:8000/index.html"
+
+echo Все серверы запущены и сайт открыт в браузере.
+pause
